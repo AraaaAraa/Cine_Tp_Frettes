c164cbd47954556733e7a4f8d4a813c9f21129b0
AraaaAraa <faraif@live.com>
2026-09-24 18:58:19 -0300

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'candy',
+        loadComponent:()=>import('./Pages/candy/candy').then((archivo)=> archivo.Candy),
+    },
+    {
+        path: 'login',
+        loadComponent:()=>import('./Pages/login/login').then((archivo)=> archivo.Login),
+    },
+    {
+        path: 'usuario',
+        loadComponent:()=>import('./Pages/usuario/usuario').then((archivo)=> archivo.Usuario),
+    },
+    //Este siempre va al final
+    {
+        path: '**',
+        loadComponent:()=>import('./Pages/error/error').then((archivo)=> archivo.Error),
+    }
+];
